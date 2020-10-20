675a95ed71d7e0f769a6dc4d75dcfe88e4c96e62
David Enciso <dpenciso@msn.com>
2020-10-20 13:47:57 -0600

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const port = process.env.PORT || 3000
+const port = 3000
 
 require('dotenv').config();
 
